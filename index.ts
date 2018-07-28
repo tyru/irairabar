e78600deb4ff27fa92fb582141a2b5c11cc32cf4
tyru <tyru.exe@gmail.com>
2018-07-29 03:27:40 +0900

    refactor: narrow scopes

--- index.ts
+++ index.ts
@@ -1,16 +1,7 @@
 import * as glMatrix from 'gl-matrix';
 
-window.requestAnimationFrame = window.requestAnimationFrame ||
-                                (window as any).mozRequestAnimationFrame ||
-                                window.webkitRequestAnimationFrame ||
-                                (window as any).msRequestAnimationFrame;
-
 const STAGE_WIDTH = 320;
 const STAGE_HEIGHT = 240;
-const bgCanvas = <HTMLCanvasElement> document.getElementById('bg');
-bgCanvas.width = STAGE_WIDTH;
-bgCanvas.height = STAGE_HEIGHT;
-const bgContext = bgCanvas.getContext('2d');
 
 class Background {
   constructor(
@@ -61,55 +52,67 @@ class KeyInput {
   }
 }
 
+window.requestAnimationFrame = window.requestAnimationFrame ||
+                                (window as any).mozRequestAnimationFrame ||
+                                window.webkitRequestAnimationFrame ||
+                                (window as any).msRequestAnimationFrame;
 
-const background = new Background(bgContext, STAGE_WIDTH, STAGE_HEIGHT, '#000000');
-const player = new Player(bgContext, '#FFFFFF');
-const key = new KeyInput();
+(() => {
+  const key = new KeyInput();
 
-window.onkeydown = (e) => {
-  switch (e.keyCode) {
-    case 37:
-      key.left = true;
-      break;
-    case 38:
-      key.up = true;
-      break;
-    case 39:
-      key.right = true;
-      break;
-    case 40:
-      key.down = true;
-      break;
+  window.onkeydown = (e) => {
+    switch (e.keyCode) {
+      case 37:
+        key.left = true;
+        break;
+      case 38:
+        key.up = true;
+        break;
+      case 39:
+        key.right = true;
+        break;
+      case 40:
+        key.down = true;
+        break;
+    }
+  };
+
+  window.onkeyup = (e) => {
+    switch (e.keyCode) {
+      case 37:
+        key.left = false;
+        break;
+      case 38:
+        key.up = false;
+        break;
+      case 39:
+        key.right = false;
+        break;
+      case 40:
+        key.down = false;
+        break;
+    }
+  };
+
+  const bgCanvas = <HTMLCanvasElement> document.getElementById('bg');
+  bgCanvas.width = STAGE_WIDTH;
+  bgCanvas.height = STAGE_HEIGHT;
+  const bgContext = bgCanvas.getContext('2d');
+
+  const background = new Background(bgContext, STAGE_WIDTH, STAGE_HEIGHT, '#000000');
+  const player = new Player(bgContext, '#FFFFFF');
+
+  function tick() {
+    player.move(key.toMat2d());
+
+    background.draw();
+    player.draw();
+
+    requestAnimationFrame(tick);
   }
-};
 
-window.onkeyup = (e) => {
-  switch (e.keyCode) {
-    case 37:
-      key.left = false;
-      break;
-    case 38:
-      key.up = false;
-      break;
-    case 39:
-      key.right = false;
-      break;
-    case 40:
-      key.down = false;
-      break;
-  }
-};
+  document.addEventListener('DOMContentLoaded', () => {
+    requestAnimationFrame(tick);
+  });
 
-function tick() {
-  player.move(key.toMat2d());
-
-  background.draw();
-  player.draw();
-
-  requestAnimationFrame(tick);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  requestAnimationFrame(tick);
-});
-
+})();
