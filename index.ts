7ae06011c50d1e5c29fb3f238c964a9ae50c0ea5
tyru <tyru.exe@gmail.com>
2018-07-29 04:14:04 +0900

    feat: blur player circle

--- index.ts
+++ index.ts
@@ -3,31 +3,63 @@ import * as glMatrix from 'gl-matrix';
 const STAGE_WIDTH = 320;
 const STAGE_HEIGHT = 240;
 
+class Color {
+  private constructor(
+    public readonly r: number,
+    public readonly g: number,
+    public readonly b: number,
+    public readonly a: number = 1,
+  ) {
+    if (r < 0 || r > 255) {
+      throw new Error(`Color: r = ${r}`);
+    }
+    if (g < 0 || g > 255) {
+      throw new Error(`Color: g = ${g}`);
+    }
+    if (b < 0 || b > 255) {
+      throw new Error(`Color: b = ${b}`);
+    }
+    if (a < 0 || a > 1) {
+      throw new Error(`Color: a = ${a}`);
+    }
+  }
+
+  public static fromRGB(r: number, g: number, b: number) {
+    return new Color(r, g, b);
+  }
+
+  toRGBAString() {
+    return `rgba(${this.r},${this.g},${this.b},${this.a})`;
+  }
+}
+
 class Background {
   constructor(
     private readonly context: CanvasRenderingContext2D,
     private readonly width: number,
     private readonly height: number,
-    private readonly color: string) { }
+    private readonly color: Color) { }
 
   draw() {
-    this.context.fillStyle = this.color;
+    this.context.fillStyle = this.color.toRGBAString();
     this.context.fillRect(0, 0, this.width, this.height);
   }
 }
 
 class Player {
-  private readonly radius: number;
   private readonly point: glMatrix.vec2;
+  private readonly radius: number;
+  private readonly actualSizePer: number;
+
   constructor(
     private readonly context: CanvasRenderingContext2D,
-    private readonly color: string,
+    private readonly color: Color,
     x: number = 0,
     y: number = 0,
-    radius: number = 10,
   ) {
     this.point = glMatrix.vec2.fromValues(x, y);
-    this.radius = 10;
+    this.radius = 8;
+    this.actualSizePer = 0.8;
   }
 
   move(trans: glMatrix.mat2d) {
@@ -35,9 +67,19 @@ class Player {
   }
 
   draw() {
-    this.context.fillStyle = this.color;
+    const actRad = this.radius / this.actualSizePer;
+    const radGrad = this.context.createRadialGradient(
+      this.point[0], this.point[1], 0,
+      this.point[0], this.point[1], actRad,
+    );
+    const { r, g, b } = this.color;
+    radGrad.addColorStop(0, `rgba(${r},${g},${b},1)`);
+    radGrad.addColorStop(0.5, `rgba(${r},${g},${b},${this.actualSizePer})`);
+    radGrad.addColorStop(1, `rgba(${r},${g},${b},0)`);
+
+    this.context.fillStyle = radGrad;
     this.context.beginPath();
-    this.context.arc(this.point[0], this.point[1], this.radius, 0, Math.PI * 2, false);
+    this.context.arc(this.point[0], this.point[1], actRad, 0, Math.PI * 2, false);
     this.context.fill();
   }
 }
@@ -102,8 +144,8 @@ window.requestAnimationFrame = window.requestAnimationFrame ||
   bgCanvas.height = STAGE_HEIGHT;
   const bgContext = bgCanvas.getContext('2d');
 
-  const background = new Background(bgContext, STAGE_WIDTH, STAGE_HEIGHT, '#000000');
-  const player = new Player(bgContext, '#FFFFFF', STAGE_WIDTH / 2, STAGE_HEIGHT - 40);
+  const background = new Background(bgContext, STAGE_WIDTH, STAGE_HEIGHT, Color.fromRGB(0, 0, 0));
+  const player = new Player(bgContext, Color.fromRGB(255, 255, 255), STAGE_WIDTH / 2, STAGE_HEIGHT - 40);
 
   function tick() {
     player.move(key.toMat2d());
